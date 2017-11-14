ca49f109c485efcfb2997cfe3bfe635bce37b108
LudoCarlu <ludoviccarlu@gmail.com>
2017-11-14 22:06:06 +0100

    MAJ des requetes
    Indent + rajout exercice complet version txt

--- TP1/Exercice 7/ex_07_05.sql
+++ TP1/Exercice 7/ex_07_05.sql
@@ -1,7 +1,12 @@
 SELECT 
-LAST_NAME,
-SALARY
+  LAST_NAME,
+  SALARY
 FROM EMPLOYEES
 WHERE
-MANAGER_ID IN (SELECT EMPLOYEE_ID FROM EMPLOYEES WHERE LAST_NAME = 'King') -- il y a aussi une femme King donc on met le IN
+  MANAGER_ID IN 
+    (
+    SELECT EMPLOYEE_ID 
+    FROM EMPLOYEES 
+    WHERE LAST_NAME = 'King'
+    ) -- il y a aussi une femme King donc on met le IN
 ;
